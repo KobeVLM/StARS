1e0db1df9be592d5523d7d57e39630aa9024826d
SimonJayed <simonjay.lugatiman@cit.edu>
2025-09-30 21:21:23 +0800

    feat: integrated database to supabase

--- stars_project/stars/settings.py
+++ stars_project/stars/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,14 +78,14 @@ WSGI_APPLICATION = 'stars.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stars_db',
-        'USER': 'root',  # XAMPP default
-        'PASSWORD': '',  # XAMPP default (empty)
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
         'OPTIONS': {
-            'sql_mode': 'STRICT_TRANS_TABLES',
+            'sslmode': 'require',
         }
     }
 }
