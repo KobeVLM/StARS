15a7a7c91de773d50152fc24db4aadbc32ece19a
SimonJayed <simonjay.lugatiman@cit.edu>
2025-10-01 02:33:14 +0800

    feat: added primary keys and unique ids

--- stars_project/stars/settings.py
+++ stars_project/stars/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,11 +78,11 @@ WSGI_APPLICATION = 'stars.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': 'postgres',
+        'USER': 'postgres',  # XAMPP default
+        'PASSWORD': 'IoeNPpa2NWrcEuqu',  # XAMPP default (empty)
+        'HOST': 'db.aafzluewbkzelfkvdegk.supabase.co',
+        'PORT': '5432',
         'OPTIONS': {
             'sslmode': 'require',
         }
