c2e22839ab6d585b51ddfdfb95dca19763ce8e3e
Kobe Vincent Laborte Marikit <74773781+KobeVLM@users.noreply.github.com>
2025-10-12 21:22:59 +0800

    fix: Email Validation (#16)
    * fix: Added Email uniqueness validation during Registration
    
    * I really don't know how to untrack this freking cache
    
    * fix: updated email validation
    
    * fix: improve email validation in user registration form
    
    * chore: update .gitignore and fix email validation in forms.py
    
    * chore: remove unwanted file
    
    * fix: improve email validation in user registration form
    
    * fix: gitignore file commit
    
    ---------
    
    Co-authored-by: SimonJayed <simonjay.lugatiman@cit.edu>

--- stars_project/accounts/forms.py
+++ stars_project/accounts/forms.py
@@ -15,6 +15,12 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists.")
+        return email
+
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}))
