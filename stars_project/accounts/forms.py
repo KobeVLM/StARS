830ab55a2b63ec3e4122ecbfeccfe075522c7922
Kyube <kobemarikit70@gmail.com>
2025-10-12 19:47:19 +0800

    fix: Added Email uniqueness validation during Registration

--- stars_project/accounts/forms.py
+++ stars_project/accounts/forms.py
@@ -14,6 +14,12 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists")
+        return email
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter username'}))
